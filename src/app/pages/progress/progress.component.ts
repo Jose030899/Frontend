934cca16d7b25e965671e7e4e6fc307264ca9468
Jose Maria Orozco Ramirez <LRS17110068@purisima.tecnm.mx>
2020-10-07 12:10:58 -0500

    app chida 2

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent  {
+ progreso: number = 85;
 
-  constructor() { }
+ get getPorcentaje(){
+  return `${this.progreso}%`;
+ }
 
-  ngOnInit(): void {
+ cambiarValor(valor: number){
+   if(this.progreso >= 100 && valor >=0){
+    return this.progreso = 100;
+   }
+
+   if(this.progreso <= 0 && valor < 0){
+    return this.progreso = 0;
+   }
+
+   this.progreso = this.progreso + valor;
+ }
+
+ onChange(nuevovalor:number){
+   if(nuevovalor >= 100){
+     this.progreso = 100;
+   }
+   if(nuevovalor <= 0){
+    this.progreso = 0;
   }
-
+ }
 }
